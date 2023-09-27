2a15b24d75389c6a38d7179370f9c1eb8cd86096
Matthew Wong <mattwong991@gmail.com>
2023-09-27 16:55:47 -0600

    Complete with checkout

--- app.js
+++ app.js
@@ -82,19 +82,65 @@ function drawTotal(total) {
 function drawCart() {
   let content = ''
   iceCream.forEach(flavor => {
-    if (flavor.quantity > 0)
-      content += `<p>${flavor.name} | Qty: ${flavor.quantity}</p>`
+    if (flavor.quantity > 0) {
+      content += `
+      <div class="col-4 my-3">${flavor.name}</div>
+      <div class="col-2 my-3">${flavor.quantity}</div>
+      <div class="col-3 my-3">$${flavor.price}</div>
+      <div class="col-3 my-3">$${flavor.quantity * flavor.price}</div>
+      `
+    }
   })
 
   toppings.forEach(topping => {
     if (topping.quantity > 0)
-      content += `<p>${topping.name} | Qty: ${topping.quantity}</p>`
+      content += `
+    <div class="col-4 my-3">${topping.name}</div>
+    <div class="col-2 my-3">${topping.quantity}</div>
+    <div class="col-3 my-3">$${topping.price}</div>
+    <div class="col-3 my-3">$${topping.quantity * topping.price}</div>
+    `
   })
+  if (!content) {
+    content = '<span class="my-3">No items are in the cart.</span>'
+  }
+  // content += `<p>${topping.name} | Qty: ${topping.quantity}</p>`
   const cartElement = document.getElementById('cart')
   //@ts-ignore
   cartElement.innerHTML = content
 }
 
+function checkout() {
+  const confirmCheckout = window.confirm('Are you sure you want to checkout?')
+  if (!confirmCheckout)
+    return
+  iceCream.forEach(flavor => flavor.quantity = 0)
+  toppings.forEach(topping => topping.quantity = 0)
+  drawCart()
+}
 
+function checkoutSA() {
+  Swal.fire({
+    title: 'Are you sure you want to checkout?',
+    text: "Double check your cart!",
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, order my ice cream!'
+  }).then((result) => {
+    if (!result.isConfirmed) {
+      return
+    }
+    Swal.fire(
+      'Ordered Successfully!',
+      'It should be ready in about 5 minutes',
+      'success'
+    )
+    iceCream.forEach(flavor => flavor.quantity = 0)
+    toppings.forEach(topping => topping.quantity = 0)
+    calculateCost()
+  })
+}
 // !SECTION
 
